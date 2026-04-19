f321c2b47cd334ad7304bb1634e5addfc0f91f20
info-webs <info@posicionamiento-webs.com>
2026-04-20 00:01:07 +0200

    v1

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://bsaas-astro.web3templates.com/",
+  integrations: [tailwind(), mdx(), sitemap()],
+});
